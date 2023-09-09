9b797cfd753f99339d4bff11ab9d3127b1971b2b
jikor1st <jikor1st@naver.com>
2023-09-10 00:25:34 +0900

    next config 수정

--- next.config.js
+++ next.config.js
@@ -1,23 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const withPlugins = require("next-compose-plugins");
-const withPWA = require("next-pwa");
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
 
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = withPlugins(
-  [
-    [
-      withPWA,
-      {
-        pwa: {
-          dest: "public",
-        },
-      },
-    ],
-    // 추가 플러그인 작성
-  ],
-  nextConfig
-);
+module.exports = withPWA({ ...nextConfig });
