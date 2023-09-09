6ad4ffa35086247f07516b30f555261b7721916f
jikor1st <jikor1st@naver.com>
2023-09-09 23:47:30 +0900

    manifetst작성 및 icon 추가, link, meta태그 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("next-pwa");
+
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withPlugins(
+  [
+    [
+      withPWA,
+      {
+        pwa: {
+          dest: "public",
+        },
+      },
+    ],
+    // 추가 플러그인 필요시 작성
+  ],
+  nextConfig
+);
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,32 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+
+        <link
+          href="images/favicons/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="images/favicons/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        {/* ios 아이콘 메타태그 */}
+        <link rel="apple-touch-icon" href="images/icons/icon-192x192.png" />
+        {/* Microsoft App 컬러 메타태그 */}
+        <meta name="msapplication-TileColor" content="#3b34ff" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
